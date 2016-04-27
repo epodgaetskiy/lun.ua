e3f413ec7834f72946cb0759ea66bf9f682c1901
Evgeniy Podgaetskiy <epodgaetskiy@gmail.com>
2016-04-27 22:59:56 +0300

    eng

--- form.php
+++ form.php
@@ -2,7 +2,7 @@
 
 if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["country"]) && isset($_POST["city"]) && isset($_POST["fb"]) && isset($_POST["vk"]) && isset($_POST["tw"]) && isset($_POST["od"])) { 
 
-	// Формируем массив для JSON ответа
+	// array to JSON
     $result = array(
     	'name' => $_POST["name"],
     	'email' => $_POST["email"],
@@ -14,7 +14,6 @@ if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["country"]) 
     	'od' => $_POST["od"]
     ); 
 
-    // Переводим массив в JSON
     echo json_encode($result); 
 }
 
