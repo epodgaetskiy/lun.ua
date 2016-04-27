423642516e6004dc088322afede6ae36e095e36a
Evgeniy Podgaetskiy <epodgaetskiy@gmail.com>
2016-04-27 22:25:36 +0300

    add structure

--- js/common.js
+++ js/common.js
@@ -4,7 +4,7 @@ var step = $(".step");
 	btnPrev = $(".btn-prev");
 	btnFinish = $(".btn-finish");
 	btnReset = $(".btn-reset");
-	sourceDate = {};
+	sourceDate = {}; 
 //view first step
 step.eq(0).addClass("active");
 stepBtn.eq(0).addClass("active");
@@ -20,200 +20,225 @@ sourceDate.Step  = function (name, email, country, city, fb, vk, tw, od){
 	this.od = od;
 }
 
-$(document).ready(function(){
+//load list country and cities
+getCountries();
+getCities();
 
-	var maxStep = 0; //fihish step
+//choice cat
+var imgStep = $(".img_block img");
+	imgFormSrc = '';
 
- 	btnNext.each( function() {
-		$(this).click( function(e) {
+imgStep.each( function() {
+	$(this).click( function(e) {
+		e.preventDefault();
 
-			maxStep++; //if field added
+		var i = imgStep.index($(this)); //indox of img
+			flag = false;
 
-			var i = btnNext.index($(this));
-				ValidationForm = false;
-			//need check form valid  !!!
-			if (i == 0) {
+		if (i == 3) {  // index 3 is dog
+			imgStep.removeClass("active");
+			imgStep.eq(i).addClass("active");
+			$(".choice-dog").addClass("noCat");
+		} else {
+			$(".choice-dog").removeClass("noCat");
+			imgStep.removeClass("active");
+			imgStep.eq(i).addClass("active");
+			imgFormSrc = imgStep.eq(i).attr("src");
+			flag = true;
+		}
 
-				var name =  document.getElementById('name');
-					email = document.getElementById('email');
-					error = document.querySelector('.error');
+	});
+});
 
-				if ( name.checkValidity() && email.checkValidity()) {
-					ValidationForm = true;
-				}
+var maxStep = 0; //fihish step
+//next btn
+btnNext.each( function() {
+	$(this).click( function(e) {
 
-				if (!name.checkValidity()) {
-    				name.classList.add("invalid");
-				} 
+		maxStep++; //if field added
 
-				if (!email.checkValidity()) {
-					error.innerHTML = "- в адресе должен быть символ @";
-    				email.classList.add("invalid");
-				}
-			} 
+		var i = btnNext.index($(this));
+			ValidationForm = false;
+		//check valid form fisrt step
+		if (i == 0) {
 
-			if (i == 1) {
+			var name =  document.getElementById('name');
+				email = document.getElementById('email');
+				error = document.querySelector('.error');
 
-				var inputCountry =  document.getElementById('inputCountry');
-					inputCity = document.getElementById('inputCity');
-
-				if ( inputCountry.checkValidity() && inputCity.checkValidity()) {
-					ValidationForm = true;
-				}
-
-				if (!inputCountry.checkValidity()) {
-    				inputCountry.classList.add("invalid");
-				} 
-
-				if (!inputCity.checkValidity()) {
-    				inputCity.classList.add("invalid");
-				}
-			}
-
-			if (i == 2) {
+			if ( name.checkValidity() && email.checkValidity()) {
 				ValidationForm = true;
 			}
 
-			if (ValidationForm) {
-				sendAjaxForm();
+			if (!name.checkValidity()) {
+				name.classList.add("invalid");
+			} 
 
-				//delete invalid style
-				//first step
-				if (i ==0 ) {
-					name.classList.remove("invalid");
-					error.innerHTML = "";
-					email.classList.remove("invalid");
-				}
-				//second step
-				if (i == 1) {
-					inputCountry.classList.remove("invalid");
-					inputCity.classList.remove("invalid");
-				}
+			if (!email.checkValidity()) {
+				error.innerHTML = "- в адресе должен быть символ @";
+				email.classList.add("invalid");
+			}
+		} 
+		//check valid form second step
+		if (i == 1) {
 
-				step.eq(i).removeClass("active");
+			var inputCountry =  document.getElementById('inputCountry');
+				inputCity = document.getElementById('inputCity');
+
+			if ( inputCountry.checkValidity() && inputCity.checkValidity()) {
+				ValidationForm = true;
+			}
+
+			if (!inputCountry.checkValidity()) {
+				inputCountry.classList.add("invalid");
+			} 
+
+			if (!inputCity.checkValidity()) {
+				inputCity.classList.add("invalid");
+			}
+		}
+
+		if (i == 2) {
+			ValidationForm = true;
+		}
+
+		if (ValidationForm) {
+			sendAjaxForm();
+			//delete invalid style
+			//first step
+			if (i ==0 ) {
+				name.classList.remove("invalid");
+				error.innerHTML = "";
+				email.classList.remove("invalid");
+			}
+			//second step
+			if (i == 1) {
+				inputCountry.classList.remove("invalid");
+				inputCity.classList.remove("invalid");
+			}
+
+			step.eq(i).removeClass("active");
+			stepBtn.eq(i).addClass("finished");
+
+			i++; //add class style f onext btn
+
+			step.removeClass("active");
+			step.eq(i).addClass("active");
+			stepBtn.removeClass("active");
+			stepBtn.eq(i).addClass("finished");
+			stepBtn.eq(i).addClass("active");
+
+			if (i == 3) {
 				stepBtn.eq(i).addClass("finished");
-
-				i++; //add class style f onext btn
-
-				step.removeClass("active");
-				step.eq(i).addClass("active");
-				stepBtn.removeClass("active");
-				stepBtn.eq(i).addClass("finished");
-				stepBtn.eq(i).addClass("active");
-
-				if (i == 3) {
-					stepBtn.eq(i).addClass("finished");
-				}
-
-			}
-			
-		});
- 	});
-
- 	btnPrev.each( function() {
-		$(this).click( function(e) {
-			e.preventDefault();
-
-			var i = btnPrev.index($(this));
-
-			if (i != 0) {
-				step.removeClass("active");
-			}
-			
-			i = i-1;
-
-			if (i >= 0) {
-				step.removeClass("active");
-				step.eq(i).addClass("active");
-				stepBtn.removeClass("active");
-				stepBtn.eq(i).addClass("active");
-			}
-			
-		});
- 	});
- 	//high navigation
- 	stepBtn.each( function() {
-		$(this).click( function(e) {
-			e.preventDefault();
-
-			var i = stepBtn.index($(this));
-
-			if (i <= maxStep) {
-				step.removeClass("active");
-				step.eq(i).addClass("active");
-				stepBtn.removeClass("active");
-				stepBtn.eq(i).addClass("active");
 			}
 
-		});
- 	});
+		}
+		
+	});
+});
+//back btn
+btnPrev.each( function() {
+	$(this).click( function(e) {
+		e.preventDefault();
 
- 	btnFinish.click(function(e){
- 		e.preventDefault();
+		var i = btnPrev.index($(this));
 
- 		if (flag) {
- 			sourceDate.img = imgFormSrc;
- 			showResult(sourceDate);
- 			$(".form-steps").addClass("hide-block");
- 			$(".show-result").addClass("show-active");
- 		}
- 	});
+		if (i != 0) {
+			step.removeClass("active");
+		}
+		
+		i = i-1;
 
- 	btnReset.click(function(e){
- 		e.preventDefault();
+		if (i >= 0) {
+			step.removeClass("active");
+			step.eq(i).addClass("active");
+			stepBtn.removeClass("active");
+			stepBtn.eq(i).addClass("active");
+		}
+		
+	});
+});
+//high navigation
+stepBtn.each( function() {
+	$(this).click( function(e) {
+		e.preventDefault();
 
- 		step.removeClass("active");
- 		stepBtn.removeClass("active");
- 		stepBtn.removeClass("finished");
- 		step.eq(0).addClass("active");
-		stepBtn.eq(0).addClass("active");
-		maxStep = 0; //fihish step
-		//delete field for form
-		$("input","#form")
-			.val("")
-			.removeAttr("checked")
-			.removeAttr("selected");
-		imgStep.removeClass("active");
-		sourceDate.Step("", "", "", "", "", "", "", "");
-		sourceDate.img= "";
-		$(".fb").text("");
-		$(".fb").parent().removeClass("show-social");
-		$(".vk").text("");
-		$(".vk").parent().removeClass("show-social");
-		$(".tw").text("");
-		$(".tw").parent().removeClass("show-social");
-		$(".od").text("");
-		$(".od").parent().removeClass("show-social");
-		$(".form-steps").removeClass("hide-block");
-		$(".show-result").removeClass("show-active");
- 	});
+		var i = stepBtn.index($(this));
+
+		if (i <= maxStep) {
+			step.removeClass("active");
+			step.eq(i).addClass("active");
+			stepBtn.removeClass("active");
+			stepBtn.eq(i).addClass("active");
+		}
+
+	});
+});
+
+btnFinish.click(function(e){
+	e.preventDefault();
+
+	if (flag) {
+		sourceDate.img = imgFormSrc;
+		showResult(sourceDate);
+		$(".form-steps").addClass("hide-block");
+		$(".show-result").addClass("show-active");
+	}
+});
+
+btnReset.click(function(e){
+	e.preventDefault();
+
+	step.removeClass("active");
+	stepBtn.removeClass("active");
+	stepBtn.removeClass("finished");
+	step.eq(0).addClass("active");
+	stepBtn.eq(0).addClass("active");
+
+	maxStep = 0; //fihish step
+	//delete field for form
+	$("input","#form")
+		.val("")
+		.removeAttr("checked")
+		.removeAttr("selected");
+
+	imgStep.removeClass("active");
+	//delete date form
+	sourceDate.Step("", "", "", "", "", "", "", "");
+	sourceDate.img= "";
+	//delete sicial date
+	$(".fb").text("");
+	$(".fb").parent().removeClass("show-social");
+	$(".vk").text("");
+	$(".vk").parent().removeClass("show-social");
+	$(".tw").text("");
+	$(".tw").parent().removeClass("show-social");
+	$(".od").text("");
+	$(".od").parent().removeClass("show-social");
+	$(".form-steps").removeClass("hide-block");
+	$(".show-result").removeClass("show-active");
 
 });
 
 function sendAjaxForm(url) {
     jQuery.ajax({
-        url: "form.php", //url страницы (form.php)
-        type: "POST", //метод отправки
-        dataType: "html", //формат данных
-        data: jQuery("#form").serialize(),  // Сеарилизуем объект
-        success: function(response) { //Данные отправлены успешно
+        url: "form.php", //url (form.php)
+        type: "POST", 
+        dataType: "html", 
+        data: jQuery("#form").serialize(), 
+        success: function(response) { 
         	result = jQuery.parseJSON(response);
         	sourceDate.Step(result.name, result.email, result.country, result.city, result.fb, result.vk, result.tw, result.od);
-        	console.log(sourceDate);
     	},
-    	error: function(response) { // Данные не отправлены
+    	error: function(response) { 
     	}
  	});
 }
 
-//load list country and cities
-getCountries();
-getCities();
-
 function getCountries() {
 	var xhr = new XMLHttpRequest();
 
-	xhr.open('GET', 'date/countries.json'); //Загрузка данных из файла date/countries.json по XMLHttpRequest.
+	xhr.open('GET', 'date/countries.json'); //Load from date/countries.json by XMLHttpRequest
 
 	xhr.onload = function(evt) {
 
@@ -237,15 +262,16 @@ function showCountries(countries) {
 
 		listCountries.appendChild(option);
 		option.value = countries[k];
-	}
-		
+		var temp = "country-";
+		option.classList.add(temp + k);
+	}		
 
 }
 
 function getCities() {
 	var xhr = new XMLHttpRequest();
 
-	xhr.open('GET', 'date/cities.json'); //Загрузка данных из файла date/countries.json по XMLHttpRequest.
+	xhr.open('GET', 'date/cities.json'); ////Load from date/cities.json by XMLHttpRequest
 
 	xhr.onload = function(evt) {
 
@@ -273,32 +299,6 @@ function showCities(cities) {
 		
 
 }
-
-//choice cat
-var imgStep = $(".img_block img");
-	imgFormSrc = '';
-
-imgStep.each( function() {
-	$(this).click( function(e) {
-		e.preventDefault();
-
-		var i = imgStep.index($(this)); //indox of img
-			flag = false;
-
-		if (i == 3) {  // index 3 is dog
-			imgStep.removeClass("active");
-			imgStep.eq(i).addClass("active");
-			$(".choice-dog").addClass("noCat");
-		} else {
-			$(".choice-dog").removeClass("noCat");
-			imgStep.removeClass("active");
-			imgStep.eq(i).addClass("active");
-			imgFormSrc = imgStep.eq(i).attr("src");
-			flag = true;
-		}
-
-	});
-});
 //write date to show-result
 function showResult(date) {
 	$(".name").text(date.name);
